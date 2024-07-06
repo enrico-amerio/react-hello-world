ab166d2b7a4c44c0b4fdaf9bf0bebc1c32f43b1a
enrico-amerio <amerio.enrico95@gmail.com>
2024-07-06 17:53:55 +0200

    [add] Btn & Alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import ListGroup from "./components/ListGroup";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
+import { useState } from "react";
+
 
 function App(){
+  const [alertVisible, setAlertVisibility] = useState(false);
   let items = [
     'Napoli',
     'Torino',
@@ -12,15 +16,21 @@ function App(){
     console.log(item)
   }
   return (
-    <div className="container">
-  <div>
-    <Alert>
-      Hello <span className="text-danger">World!</span> 
-    </Alert>
-  </div>  
-  <div>
-    <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
-  </div>
+  <div className="container">
+    <div>
+      {
+        alertVisible && 
+        <Alert onClose={()=> setAlertVisibility(false)}>
+          Hello <span className="text-danger">World!</span> 
+        </Alert>
+      }
+      <Button onClick={() => setAlertVisibility(true)}>
+        Do not Click
+      </Button>
+    </div>  
+    <div>
+      <ListGroup items={items} heading="Cities" onSelectItem={handleSelectItem}/>
+    </div>
   </div>
 )
 }
